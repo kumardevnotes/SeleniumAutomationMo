450c1e6173904913e39c2d5d72e62a7d075baa23
kumardevnotes <kumar.thoudishetti@gmail.com>
2022-12-17 16:50:20 +0530

    added

--- src/test/java/com/p/Selenium/SeleniumDemoChrome1.java
+++ src/test/java/com/p/Selenium/SeleniumDemoChrome1.java
@@ -2,7 +2,7 @@ package com.p.Selenium;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumDemoChrome {
+public class SeleniumDemoChrome1 {
 	
 	public static void main(String[] args) throws Exception {
 		//to get the root folder of the application
